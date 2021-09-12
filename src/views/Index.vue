67c7f71dce6e95a0f63d273c3ac0c4e438189ff0
flls <2498441970@qq.com>
2021-09-12 15:16:31 +0800

    完善了红包部分

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -10,8 +10,21 @@
   </div>
 
   <div class="gift-index">
-    <div class="meituan"></div>
-    <div class="elemang"></div>
+    <div class="elemang">
+      <img src="../assets/img/饿了么.png" alt="">
+      <div style="margin-left:2vw;display:block;">
+        <p style="font-size:4vw;color:white">饿了么外卖红包</p>
+        <p style="font-size:5vw;font-weight:700;margin-left:-2vw;color:white"> 最高领<em style="color:#f8cb4a">66</em>元 </p>
+      </div>      
+    </div>
+    <div class="meituan">
+      <img src="../assets/img/美团.jpg" alt="">
+      <div style="margin-left:2vw;display:block;">
+        <p style="font-size:4vw;margin-left:-1.5vw">美团外卖红包</p>
+        <p style="font-size:5vw;font-weight:700"> 最高领<em style="color:red">56</em>元 </p>
+      </div>      
+    </div>
+    
   </div>
 </div>
   
@@ -24,6 +37,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
+  
   .index {
     width: 100vw;
     height: 100vh;
@@ -39,20 +53,36 @@ export default {
    height: 15vh;
    width: 100vw;
    .meituan{
+     display: flex;
+     align-items: center;
      margin-top: 1vh;
-     margin-left: 1vw;
-     height: 13vh;
+     margin-left: 2vw;
+     height: 23vw;
      width: 48vw;
-     background-color: red;
+     background-color: #f8cb4a;
      border-radius: 2vh;
+     img{
+       margin-left: 2vw;
+       width: 16vw;
+       height: 16vw;
+       border-radius: 8vw;
+     }
    }
    .elemang{
+     display: flex;
+     align-items: center;
      margin-top: 1vh;
      margin-left: 1vw;
-     height: 13vh;
+     height: 23vw;
      width: 48vw;
-     background-color: blue;
+     background-color: #06a9ff;
      border-radius: 2vh;
+      img{
+       margin-left: 2vw;
+       width: 16vw;
+       height: 16vw;
+       border-radius: 8vw;
+     }
    }
  }
 
